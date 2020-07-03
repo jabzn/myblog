9bb008fb587b2cc4a5a76675b5961e9d386ea99b
JabzN <jabalnoer12@gmail.com>
2020-07-03 23:30:47 +0800

    Repair Contact Button

--- resources/views/layouts/_contact-form.blade.php
+++ resources/views/layouts/_contact-form.blade.php
@@ -69,5 +69,5 @@
         <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
     @enderror
 
-    <button type="submit" class="bg-blue-400 py-2 px-4 mt-2 rounded hover:bg-blue-600 duration-150 text-white font-bold">Post Comment</button>
+    <button type="submit" class="bg-blue-400 py-2 px-4 mt-2 rounded hover:bg-blue-600 duration-150 text-white font-bold">Send Messeage</button>
 </div>
